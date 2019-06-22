59723c936949aac94a4e415c4030ff04b9736354
rduran408 <42043075+rduran408@users.noreply.github.com>
2019-06-22 13:43:26 -0700

    Delete schema.lua

--- schema.lua
+++
@@ -1,9 +0,0 @@
-return {
-  no_consumer = true,
-  fields = {
-    url = { required = true, type = "url" },
-    connect_timeout = { default = 10000, type = "number" },
-    send_timeout = { default = 60000, type = "number" },
-    read_timeout = { default = 60000, type = "number" }
-  }
-}
